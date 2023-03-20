cb90a575382d07f03ea4086a548fda93e8852fb0
Christian Wendt <christian.wendt@eduart-robotik.com>
2023-03-20 13:51:12 +0000

    Add namespace for specific robot.

--- launch/robot_remote_control.launch.py
+++ launch/robot_remote_control.launch.py
@@ -5,18 +5,19 @@ from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, EnvironmentVariable, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    package_path = get_package_share_path('edu_robot_control')
-    parameter_file = os.path.join(
+    package_path = FindPackageShare('edu_robot_control')
+    parameter_file = PathJoinSubstitution([
       package_path,
       'parameter',
       'remote_control.yaml'
-    )
+    ])
 
     joy_node = Node(
       package='joy',
@@ -24,13 +25,15 @@ def generate_launch_description():
       parameters=[
         {'autorepeat_rate': 20.0},
         {'coalesce_interval_ms' : 50}
-      ]
+      ],
+      namespace=EnvironmentVariable('EDU_ROBOT_NAMESPACE', default_value="eduard")
     )
 
     remote_control_node = Node(
       package='edu_robot_control',
       executable='remote_control',
-      parameters= [parameter_file]
+      parameters=[parameter_file],
+      namespace=EnvironmentVariable('EDU_ROBOT_NAMESPACE', default_value="eduard")
     )
 
     return LaunchDescription([
